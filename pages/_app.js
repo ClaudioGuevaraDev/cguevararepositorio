66a71dc0257f0f61290dd3fc92e7bb60dc2af2c6
Claudio Guevara <claudio.guevara.dev@gmail.com>
2024-08-01 19:35:49 -0400

    añadiendo metatagas

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 
 import { NextUIProvider } from '@nextui-org/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Head from 'next/head';
 import { Toaster } from 'sonner';
 
 import Background from '@/components/layout/Background';
@@ -20,6 +21,12 @@ export default function App({ Component, pageProps }) {
       <NextUIProvider>
         <div className="w-full h-full min-h-screen">
           <Background />
+
+          <Head>
+            <title>cguevararepositorio</title>
+            <meta name="description" content="Mis  recursos para desarrollo de software"></meta>
+            <meta name="author" content="Claudio Guevara"></meta>
+          </Head>
 
           <Component {...pageProps} />
 
